3000665f5646e2513b902423497ea987dc41a8ae
caspii <caspar.wrede@gmail.com>
2016-02-25 12:16:53 +0100

    Data now pulled from Google Spreadsheet

--- app.js
+++ app.js
@@ -4,38 +4,35 @@ $(function() {
   ajaxURL = "/user_stories.csv"
 
 
+  var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1HZsaJEXOp0dUEeVp9CdUmHlv16QYmqP_jq7wCVxLgh4/pubhtml?gid=0&single=true';
 
 
-  $.ajax({
-       url: ajaxURL,
-       success: function(data_csv) {
-         stories = $.csv.toArrays(data_csv);
-         //console.log(a)
-         for (i = 1; i < stories.length; i++) {
-           createSlide(stories[i])
-         }
+  Tabletop.init( { key: public_spreadsheet_url,
+                       callback:  processData,
+                       simpleSheet: true } )
 
-         // Finished. Add Markdown to DOM
-         window.markdown_string += ("class: center, middle\n # THE END")
-         $( "#source" ).append(window.markdown_string)
+    function processData(stories, tabletop) {
+        //alert("Successfully processed!")
+        console.log(stories);
+        for (i = 1; i < stories.length; i++) {
+          createSlide(stories[i])
+        }
 
-         // Create slideshow
-         var slideshow = remark.create();
+        // Finished. Add Markdown to DOM
+        window.markdown_string += ("class: center, middle\n # THE END")
+        $( "#source" ).append(window.markdown_string)
 
-       },
-       error: function(xhr, ajaxOptions, thrownError) {
-         console.log(thrownError)
-       }
-     });
-
+        // Create slideshow
+        var slideshow = remark.create();
+    }
 
 });
 
 
-function createSlide(story_array){
-  window.markdown_string += "## ID: " + story_array[0] + " // PRIO: " + story_array[1] + " <hr>\n"
-  window.markdown_string += "# As a " + story_array[2] + ", I want to "
-  window.markdown_string += story_array[3] + " so that " + story_array[4] + "\n"
-  window.markdown_string += story_array[5] + "\n"
+function createSlide(story_obj){
+  window.markdown_string += "## ID: " + story_obj['ID'] + " // PRIO: " + story_obj['Priority'] + " <hr>\n"
+  window.markdown_string += "# As a " + story_obj['As a'] + ", I want to "
+  window.markdown_string += story_obj['I want to'] + " so that " + story_obj['so that'] + "\n"
+  window.markdown_string += story_obj['Notes'] + "\n"
   window.markdown_string += '---\n'
 }

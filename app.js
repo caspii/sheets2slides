880541412ba85be41c9866dab1309809380f1ae6
caspii <caspar.wrede@gmail.com>
2016-02-25 18:03:41 +0100

    Completed public page, everything working

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 $(function() {
-  console.log( 'ready!' );
-  window.markdown_string = ("class: center, middle\n\n # My User Stories\n\n---\n") // Global Variable
 
-  var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1HZsaJEXOp0dUEeVp9CdUmHlv16QYmqP_jq7wCVxLgh4/pubhtml?gid=0&single=true';
+  window.markdown_string = ("class: center, middle\n\n # My User Stories\n\n---\n")
 
-  Tabletop.init( { key: public_spreadsheet_url,
-                       callback:  processData,
-                       simpleSheet: true } )
-
-    function processData(stories, tabletop) {
-        //alert("Successfully processed!")
-        console.log(stories);
-        for (i = 1; i < stories.length; i++) {
-          createSlide(stories[i])
-        }
-
-        // Finished. Add Markdown to DOM
-        window.markdown_string += ("class: center, middle\n # THE END")
-        $( "#source" ).append(window.markdown_string)
-
-        // Create slideshow
-        var slideshow = remark.create();
-    }
+  $('#go').on('click', function () {
+    url = $('#publishURL').val()
+    $('#welcome').hide()
+    $('#waiting').show()
+    Tabletop.init( { key: url, callback:  processData, simpleSheet: true } )
+    console.log("Initialised")
+  })
 
 });
+
 
 function createSlide(story_obj){
   window.markdown_string += "## ID: " + story_obj['ID'] + " // PRIO: " + story_obj['Priority'] + " <hr>\n"
@@ -31,4 +19,18 @@ function createSlide(story_obj){
   window.markdown_string += story_obj['I want to'] + " so that " + story_obj['so that'] + "\n"
   window.markdown_string += story_obj['Notes'] + "\n"
   window.markdown_string += '---\n'
+}
+
+function processData(stories, tabletop) {
+  for (i = 1; i < stories.length; i++) {
+    createSlide(stories[i])
+  }
+
+  // Finished. Add Markdown to DOM
+  window.markdown_string += ("class: center, middle\n # THE END")
+  $( "#source" ).append(window.markdown_string)
+
+  $('#waiting').hide()
+  // Create slideshow
+  var slideshow = remark.create();
 }
